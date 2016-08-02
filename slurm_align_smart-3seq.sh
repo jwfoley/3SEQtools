5ee6663f357332df3c01af38b74f5942b23c2cc4
jwfoley <joe+github@jwfoley.com>
2016-08-02 13:25:04 -0700

    set N_mismatch

--- slurm_align_smart-3seq.sh
+++ slurm_align_smart-3seq.sh
@@ -2,7 +2,7 @@
 
 # given a list of single-end Illumina FASTQ files and a reference genome, perform the special preprocessing for Smart-3SEQ, align each file to the reference, then index the BAM output
 # processes Smart-3SEQ data by trimming first $N_N+$N_G bases: $N_N (the random UMI) is appended to the read name, and $N_G (the G overhang) is discarded
-# also removes A-tail and anything after it, defining A-tail as a run of $N_A A's with mismatches allowed
+# also removes A-tail and anything after it, defining A-tail as a run of $N_A A's with $N_mismatch mismatches allowed
 # call from directory where you want results to go
 
 samtools_path=samtools
@@ -16,6 +16,7 @@ N_thread=8
 N_N=5
 N_G=3
 N_A=8
+N_mismatch=1
 star_options='--outFilterIntronMotifs RemoveNoncanonicalUnannotated --outFilterType BySJout --outFilterMultimapNmax 1 --alignSJoverhangMin 8 --alignSJDBoverhangMin 1 --outFilterMismatchNmax 999 --alignIntronMin 20 --alignIntronMax 1000000 --alignMatesGapMax 1000000' # ENCODE options per manual, except no multimappers reported
 time='1:00:00'
 mail_type='FAIL'
@@ -40,7 +41,7 @@ do
 		set -euo pipefail
 		module load $modules
 		cd $tmp_dir
-		$unzip_path $fastq | $umi_trim_path -u $N_N -g $N_G -p $N_A 2> $wd/$rootname\_trim.log | $star_path --genomeDir $genome_dir --readFilesIn /dev/stdin --runThreadN $N_thread --outSAMtype BAM SortedByCoordinate --outStd BAM_SortedByCoordinate --outBAMcompression 0 $star_options | $dedup_path 2> $wd/$rootname\_umi.log | tee $wd/$rootname.bam | $samtools_path index /dev/stdin $wd/$rootname.bai
+		$unzip_path $fastq | $umi_trim_path -u $N_N -g $N_G -p $N_A -m $N_mismatch 2> $wd/$rootname\_trim.log | $star_path --genomeDir $genome_dir --readFilesIn /dev/stdin --runThreadN $N_thread --outSAMtype BAM SortedByCoordinate --outStd BAM_SortedByCoordinate --outBAMcompression 0 $star_options | $dedup_path 2> $wd/$rootname\_umi.log | tee $wd/$rootname.bam | $samtools_path index /dev/stdin $wd/$rootname.bai
 		cp Log.final.out $wd/$rootname\_star.log
 " | sbatch --cpus-per-task=$N_thread --job-name=$rootname\_star --output=$rootname\_star_job.log --time=$time --mail-type=$mail_type
 done
