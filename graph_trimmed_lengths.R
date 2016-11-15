53d7b9a8c8083d32a26b1023db1f584f273c0d1a
Joe Foley <jwfoley+git@stanford.edu>
2016-11-14 17:13:17 -0800

    now a single image with a facet_wrap

--- graph_trimmed_lengths.R
+++ graph_trimmed_lengths.R
@@ -7,15 +7,22 @@ library(scales)
 
 UMI.LENGTH <- 5
 
-plots <- lapply(commandArgs(trailingOnly = T), function(log.file) {
-	lengths <- read.table(log.file, skip = 6 + UMI.LENGTH, col.names = c("length", "reads"))
-	ggplot(lengths, aes(length, reads)) + 
-		geom_area() +
-		ggtitle(log.file) +
-		scale_y_continuous(label = comma)
-})
+files <- commandArgs(trailingOnly = T)
+
+lengths <- do.call(rbind, lapply(files, function(file) {
+	result <- read.table(file, skip = 6 + UMI.LENGTH, col.names = c("length", "reads"))
+	result$proportion <- result$reads / sum(result$reads)
+	result$file <- file
+	return(result)
+}))
+
 
 pdf("insert_lengths.pdf", width = 10, height = 7.5)
-plots
+ggplot(lengths, aes(length, proportion)) +
+		facet_wrap(~ file) + 
+		geom_area() +
+		xlab("insert length (nt)") +
+		ylab("reads") +
+		scale_y_continuous(label = percent)
 dev.off()
 
