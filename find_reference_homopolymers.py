d7d67a495f95b1146c24323681ab45b2ac384d2f
Joe Foley <jwfoley+git@stanford.edu>
2017-02-16 17:13:40 -0800

    not working yet but on the right track

--- find_reference_homopolymers.py
+++ find_reference_homopolymers.py
@@ -18,17 +18,16 @@ CHR_HEADER = re.compile('> *(.+)')
 
 parser = argparse.ArgumentParser(description = 'given reference sequence in FASTA format, produce a list in BED format of homopolymers of a given base')
 parser.add_argument('-l', '--length', action = 'store', type = int, default = 10, help = 'minimum length of homopolymer')
-parser.add_argument('-b', '--base', action = 'store', type = str, default = 'A', help = 'target base')
+parser.add_argument('-b', '--base', action = 'store', type = str, default = 'A', choices = sorted(COMPLEMENTS.keys()), help = 'target base')
 parser.add_argument('in_fastq', action = 'store', nargs = '?', type = argparse.FileType('r'), default = sys.stdin)
 parser.add_argument('out_bed', action = 'store', nargs = '?', type = argparse.FileType('w'), default = sys.stdout)
 args = parser.parse_args()
 
-assert len(args.base) == 1
 target_base = args.base.upper()
-#try:
-#	complement = COMPLEMENTS[target_base]
-#except KeyError:
-#	raise NotImplementedError(('unknown base %s; options: ' % target_base) + ' '.join(COMPLEMENTS.keys()))
+try:
+	complement = COMPLEMENTS[target_base]
+except KeyError:
+	raise NotImplementedError(('unknown base %s; options: ' % target_base) + ' '.join(COMPLEMENTS.keys()))
 
 
 def read_base (fastq): # simple generator that spits out (chr, pos, base) one at a time
@@ -47,43 +46,48 @@ def read_base (fastq): # simple generator that spits out (chr, pos, base) one at
 				current_pos += 1
 				yield (chrom, current_pos, base.upper())
 
-def output_region (chrom, start, end): # write a region in BED format
-	args.out_bed.write('%s\t%i\t%i\n' % (chrom, start - 1, end))
+def output_region (chrom, strand, start, end): # write a region in BED format
+	args.out_bed.write('%s\t%i\t%i\t\t\t%s\n' % (chrom, start - 1, end, '+-'[strand]))
 
-def process_region (chrom, start, region): # find the end of a region and output if it qualifies; assume the region starts on a target base
-	assert(region[0])
-	region_length = len(region) - 1 + region[-1] # subtract 1 if the final base is a mismatch (if it is, we can assume the previous base is a match)
-	if region_length >= args.length:
-		output_region(chrom, start, start + region_length - 1)
-		for i in range(region_length): sys.stderr.write(str(int(region[i]))) # test
-		sys.stderr.write('\n') # test
+def process_region (chrom, strand, start, length, last_base_match): # find the length of a region and output if it qualifies; assume the region starts on a target base
+	length = length - 1 + last_base_match # shorten if the last base is a mismatch (which implies the previous base is not)
+	if length >= args.length: output_region(chrom, strand, start, start + length - 1)
 
 
-current_region = []
+# strands processed separately, as region_start[strand] etc. where 0 = forward, 1 = reverse
 current_chrom = None
-region_start = None
+region_start = [None, None]
+region_length = [0, 0]
+last_base_match = [False, False]
 
 for chrom, pos, base in read_base(args.in_fastq):
 	if chrom != current_chrom: # new chromosome
-		if len(current_region) != 0: process_region(current_chrom, region_start, current_region)		
+		for strand in (0, 1):
+			process_region(chrom, strand, region_start[strand], region_length[strand], last_base_match[strand])
+			region_start[strand] = None
+			region_length[strand] = 0
+			last_base_match[strand] = False
 		current_chrom = chrom
-		region_start = None
-		current_region.clear()
 	
-	base_bool = (base == target_base)
+	match = (base == target_base, base == complement)
 	
-	if len(current_region) == 0:
-		if base_bool: # beginning of region
-			current_region.append(base_bool)
-			region_start = pos
-	
-	else:
-		if not base_bool and not current_region[-1]: # end of region
-			process_region(chrom, region_start, current_region[:-1])
-			current_region.clear()
-			region_start = None
-		else: # region continues
-			current_region.append(base_bool)
+	for strand in (0, 1):		
+		if region_length[strand] == 0:
+			if match[strand]: # beginning of region
+				region_start[strand] = pos
+				region_length[strand] = 1
+				last_base_match[strand] = True
+		
+		else:
+			if not match[strand] and not last_base_match[strand]: # end of region
+				process_region(chrom, strand, region_start[strand], region_length[strand], last_base_match[strand])
+				region_start[strand] = None
+				region_length[strand] = 0
+				last_base_match[strand] = False
+			else: # region continues
+				region_length[strand] += 1
+				last_base_match[strand] = match[strand]
 
-if len(current_region) != 0: process_region(current_chrom, region_start, current_region)
+for strand in (0, 1):
+	process_region(chrom, strand, region_start[strand], region_length[strand], last_base_match[strand])
 
