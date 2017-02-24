34a44002fb42b1e1bad28943d199234f4d45f44b
Joe Foley <jwfoley+git@stanford.edu>
2017-02-24 10:59:28 -0800

    truncation argument

--- align_smart-3seq.sh
+++ align_smart-3seq.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 # given a list of single-end Illumina FASTQ files and a reference genome, perform the special preprocessing for Smart-3SEQ, align each file to the reference, then index the BAM output
+# optionally truncates reads to a specified length before further processing, to simulate results from sequencing fewer cycles
 # processes Smart-3SEQ data by trimming first $N_N+$N_G bases: $N_N (the random UMI) is appended to the read name, and $N_G (the G overhang) is discarded
 # call from directory where you want results to go
 # uses shared memory option in STAR; you may need to increase the kernel's shared memory limits (e.g. "sysctl -w kernel.shmmax=34359738368 kernel.shmall=8388608" in Linux)
@@ -18,9 +19,21 @@ N_A=8
 N_mismatch=1
 star_options='--outFilterIntronMotifs RemoveNoncanonicalUnannotated --outFilterType BySJout --outFilterMultimapNmax 1 --alignSJoverhangMin 8 --alignSJDBoverhangMin 1 --outFilterMismatchNmax 999 --alignIntronMin 20 --alignIntronMax 1000000 --alignMatesGapMax 1000000 --clip3pAdapterSeq AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA --clip3pAdapterMMp 0.2' # ENCODE options per manual, except no multimappers reported and poly(A) clipped
 
+truncate_arg=''
+while getopts ":t:" opt
+do
+	case $opt in
+		t)
+			if [[ $OPTARG != +([0-9]) ]]; then echo "error: $OPTARG is not a valid length to truncate" >&2; exit 1; fi
+			truncate_arg="-L $OPTARG"
+			;;
+	esac
+done
+shift $((OPTIND-1))
+
 if [ ! -n "$2" ]
 then
-	echo "usage: $(basename $0) genome_dir file1.fastq.gz file2.fastq.gz file3.fastq.gz ..."
+	echo "usage: $(basename $0) [-t length] genome_dir file1.fastq.gz file2.fastq.gz file3.fastq.gz ..."
 	exit 1
 fi
 
@@ -46,7 +59,7 @@ do
 	
 	cd $tmp_dir
 	$unzip_path $fastq |
-		$umi_trim_path -u $N_N -g $N_G -p $N_A -m $N_mismatch 2> $wd/$rootname.trim.log |
+		$umi_trim_path -u $N_N -g $N_G -p $N_A -m $N_mismatch $truncate_arg 2> $wd/$rootname.trim.log |
 		$star_path --genomeLoad LoadAndKeep --genomeDir $genome_dir --readFilesIn /dev/stdin --runThreadN $N_thread --outSAMtype BAM SortedByCoordinate --outStd BAM_SortedByCoordinate --outBAMcompression 10 --limitBAMsortRAM $bam_mem $star_options |
 		tee $wd/$rootname.bam |
 		$samtools_path index /dev/stdin $wd/$rootname.bai
--- slurm_align_smart-3seq.sh
+++ slurm_align_smart-3seq.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 # given a list of single-end Illumina FASTQ files and a reference genome, perform the special preprocessing for Smart-3SEQ, align each file to the reference, then index the BAM output
+# optionally truncates reads to a specified length before further processing, to simulate results from sequencing fewer cycles
 # processes Smart-3SEQ data by trimming first $N_N+$N_G bases: $N_N (the random UMI) is appended to the read name, and $N_G (the G overhang) is discarded
 # call from directory where you want results to go
 
@@ -20,10 +21,21 @@ star_options='--outFilterIntronMotifs RemoveNoncanonicalUnannotated --outFilterT
 time='1:00:00'
 mail_type='FAIL'
 
+truncate_arg=''
+while getopts ":t:" opt
+do
+	case $opt in
+		t)
+			if [[ $OPTARG != +([0-9]) ]]; then echo "error: $OPTARG is not a valid length to truncate" >&2; exit 1; fi
+			truncate_arg="-L $OPTARG"
+			;;
+	esac
+done
+shift $((OPTIND-1))
 
 if [ ! -n "$2" ]
 then
-	echo "usage: $(basename $0) genome_dir file1.fastq.gz file2.fastq.gz file3.fastq.gz ..."
+	echo "usage: $(basename $0) [-t length] genome_dir file1.fastq.gz file2.fastq.gz file3.fastq.gz ..."
 	exit 1
 fi
 
@@ -41,13 +53,13 @@ do
 		module load $modules
 		cd $tmp_dir
 		$unzip_path $fastq |
-			$umi_trim_path -u $N_N -g $N_G -p $N_A -m $N_mismatch 2> $wd/$rootname.trim.log |
+			$umi_trim_path -u $N_N -g $N_G -p $N_A -m $N_mismatch $truncate_arg 2> $wd/$rootname.trim.log |
 			$star_path --genomeDir $genome_dir --readFilesIn /dev/stdin --runThreadN $N_thread --outSAMtype BAM SortedByCoordinate --outStd BAM_SortedByCoordinate --outBAMcompression 0 $star_options |
 			$dedup_path 2> $wd/$rootname.dedup.log |
 			tee $wd/$rootname.bam |
 			$samtools_path index /dev/stdin $wd/$rootname.bai
 		touch $wd/$rootname.bai
 		cp Log.final.out $wd/$rootname.align.log
-" | sbatch --cpus-per-task=$N_thread --job-name=$rootname\_align --output=$rootname.job.log --time=$time --mail-type=$mail_type
+" #| sbatch --cpus-per-task=$N_thread --job-name=$rootname\_align --output=$rootname.job.log --time=$time --mail-type=$mail_type
 done
 
