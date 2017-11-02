bb26840ccf1691781f8fc7acc6d8429e4208b8fa
Joe Foley <jwfoley@stanford.edu>
2017-11-01 17:14:12 -0700

    --no-rlog

--- make_expression_table.R
+++ make_expression_table.R
@@ -3,18 +3,24 @@
 # given a group of BAM files from 3SEQ, make an Excel-compatible file containing read counts, transcripts per million (TPM), and DESeq2-normalized (blind rlog) gene expression values
 # assumes an Ensembl/GENCODE GTF annotation file
 
-TMP.DIR = "/tmp" # location for featureCounts temporary files
+NO.RLOG.FLAG <- "--no-rlog" # use this flag to disable calculating rlog since it can take a long time
+TMP.DIR <- "/tmp" # location for featureCounts temporary files
 
 
 command.args <- commandArgs(trailingOnly = T)
-if (length(command.args) < 3) stop("usage: make_expression_table.R annotation.gtf library1.bam library2.bam ...")
+if (length(command.args) < 3) stop(paste0("usage: make_expression_table.R [", NO.RLOG.FLAG, "] annotation.gtf library1.bam library2.bam ..."))
+do.rlog <- ! "--no-rlog" %in% command.args
+if (! do.rlog) command.args <- command.args[command.args != NO.RLOG.FLAG]
 
 library(parallel)
 library(WriteXLS)
 library(Rsubread)
 library(biomaRt)
-library(BiocParallel)
-library(DESeq2)
+
+if (do.rlog) {
+	library(BiocParallel)
+	library(DESeq2)
+}
 
 ensembl = useMart("ENSEMBL_MART_ENSEMBL", dataset = "hsapiens_gene_ensembl", host = "uswest.ensembl.org")
 
@@ -35,7 +41,6 @@ feature.counts <- featureCounts(
 )
 
 gene.counts <- feature.counts$counts
-colnames(gene.counts) <- NULL # rlogTransformation doesn't work if there are column names
 
 ensembl.gene.id <- sub("\\..*", "", rownames(gene.counts))
 gene.name.table <- getBM(
@@ -47,25 +52,35 @@ gene.name.table <- getBM(
 rownames(gene.name.table) <- gene.name.table$ensembl_gene_id
 gene.name = gene.name.table[ensembl.gene.id, "external_gene_name"]
 
-dds <- DESeq(DESeqDataSetFromMatrix(gene.counts, colData = data.frame(1:ncol(gene.counts)), ~ 1), fitType = "local", parallel = T, BPPARAM = MulticoreParam(workers = detectCores())) # eats a lot of memory!
-gene.rlogs <- assay(rlog(dds, blind = F)) # takes a long time! but slightly faster than rlog directly on the counts
+if (do.rlog) {
+	colnames(gene.counts) <- NULL # rlogTransformation doesn't work if there are column names
+	dds <- DESeq(DESeqDataSetFromMatrix(gene.counts, colData = data.frame(1:ncol(gene.counts)), ~ 1), fitType = "local", parallel = T, BPPARAM = MulticoreParam(workers = detectCores())) # eats a lot of memory!
+	gene.rlogs <- assay(rlog(dds, blind = F)) # takes a long time! but slightly faster than rlog directly on the counts
+	colnames(gene.rlogs) <- library.names
+	rownames(gene.rlogs) <- rownames(gene.counts)
+}
+
 colnames(gene.counts) <- library.names
-colnames(gene.rlogs) <- library.names
-rownames(gene.rlogs) <- rownames(gene.counts)
 
 gene.tpm <- 1E6 * sweep(gene.counts, 2, colSums(gene.counts), "/")
 
 
 save.image("make_expression_table.RData")
 
+sheets <- list(
+	data.frame("Ensembl ID" = ensembl.gene.id, "gene name" = gene.name, gene.counts, check.names = F),
+	data.frame("Ensembl ID" = ensembl.gene.id, "gene name" = gene.name, round(gene.tpm, 1), check.names = F)
+)
+sheet.names <- c("count", "TPM")
+if (do.rlog) {
+	sheets[[3]] <- data.frame("Ensembl ID" = ensembl.gene.id, "gene name" = gene.name, round(gene.rlogs, 3), check.names = F)
+	sheet.names[3] <- "rlog"
+}
+
 WriteXLS(
-	list(
-		data.frame("Ensembl ID" = ensembl.gene.id, "gene name" = gene.name, gene.counts, check.names = F),
-		data.frame("Ensembl ID" = ensembl.gene.id, "gene name" = gene.name, round(gene.tpm, 1), check.names = F),
-		data.frame("Ensembl ID" = ensembl.gene.id, "gene name" = gene.name, round(gene.rlogs, 3), check.names = F)
-	),
+	sheets,
 	ExcelFileName =  "gene_expression.xlsx",
-	SheetNames =     c("count", "TPM", "rlog"),
+	SheetNames =     sheet.names,
 	AdjWidth =       T,
 	BoldHeaderRow =  T,
 	FreezeRow =      1,
