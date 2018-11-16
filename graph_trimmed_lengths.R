66c245f40fadf0dc4f9d43b30039d8ded5c9aad4
Joe Foley <jwfoley@stanford.edu>
2018-11-16 12:30:59 -0800

    clean up library names

--- graph_trimmed_lengths.R
+++ graph_trimmed_lengths.R
@@ -6,21 +6,24 @@ library(ggplot2)
 library(scales)
 
 TABLE.HEADER <- "trimmed read length counts"
+EXPECTED.FILE.SUFFIX <- "\\.trim\\.log$" # regular expression
 
 files <- commandArgs(trailingOnly = T)
 
 lengths <- do.call(rbind, lapply(files, function(file) {
 	raw.file <- readLines(file)
-	result <- read.table(textConnection(raw.file), skip = which(raw.file == TABLE.HEADER), col.names = c("length", "reads"))
+	result <- cbind(
+		library = sub(EXPECTED.FILE.SUFFIX, "", basename(file)),
+		read.table(textConnection(raw.file), skip = which(raw.file == TABLE.HEADER), col.names = c("length", "reads"))
+	)
 	result$proportion <- result$reads / sum(result$reads)
-	result$file <- file
 	return(result)
 }))
 
 
 pdf("insert_lengths.pdf", width = 10, height = 7.5)
 ggplot(lengths, aes(length, proportion)) +
-		facet_wrap(~ file) + 
+		facet_wrap(~ library) + 
 		geom_area() +
 		xlab("insert length (nt)") +
 		ylab("reads") +
