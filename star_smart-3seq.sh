7f4f4c0f4180043766d663055064489f666ceabd
Joe Foley <jwfoley@stanford.edu>
2020-06-05 12:18:53 -0700

    remove deduplication

--- star_smart-3seq.sh
+++ star_smart-3seq.sh
@@ -6,8 +6,7 @@
 # writes output files to current working directory
 # STAR requires a lot of memory, e.g. about 25 GB for the human genome
 # uses shared memory option in STAR; you may need to increase the kernel's shared memory limits (e.g. "sysctl -w kernel.shmmax=34359738368 kernel.shmall=8388608" in Linux)
-# creates temporary BAM files and then deduplicates them all in parallel
-# or you can disable deduplication with '-d'
+
 
 N_thread=$(nproc)
 unzip_path='pigz -dc'
@@ -25,18 +24,15 @@ star_options=(
 	'--limitBAMsortRAM 2147483648' # maximum bytes of RAM to use for BAM sorting (in addition to the memory usage of the reference index!)
 	'--outBAMcompression 10'
 	'--outFilterMultimapNmax 1'
-	'--outFilterMismatchNmax 999' # don't exclude alignments with too many mismatches
+	'--outFilterMismatchNmax 999' # don't filter alignments with too many mismatches
 	'--clip3pAdapterSeq AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA' # trim poly(A)
 	'--clip3pAdapterMMp 0.2' # trim poly(A) leniently
 )
 samtools_path=samtools
-parallel_path=parallel
-dedup_command="$HOME/umi-dedup/dedup.py -qs"
 
 
 truncate_arg=''
-dedup=true
-while getopts ":n:g:t:d" opt
+while getopts ":n:g:t" opt
 do
 	case $opt in
 		n)
@@ -51,16 +47,13 @@ do
 			if [[ $OPTARG != +([0-9]) ]]; then echo "error: $OPTARG is not a valid length to truncate" >&2; exit 1; fi
 			truncate_arg="-L $OPTARG"
 			;;
-		d)
-			dedup=false
-			;;
 	esac
 done
 shift "$((OPTIND-1))"
 
 if [ ! -n "$2" ]
 then
-	echo "usage: $(basename $0) [-n umi_length] [-g discard_length] [-t truncate_length] [-d] genome_dir file1.fastq.gz file2.fastq.gz file3.fastq.gz ..." >&2
+	echo "usage: $(basename $0) [-n umi_length] [-g discard_length] [-t truncate_length] genome_dir file1.fastq.gz file2.fastq.gz file3.fastq.gz ..." >&2
 	exit 1
 fi
 
@@ -72,34 +65,19 @@ shift 1
 
 set -euo pipefail
 
-if $dedup
-then
-	loop_suffix='.bam_tmp'
-	loop_index_command='cat > /dev/null'
-	loop_touch_command=':'
-	compression_level=0
-else
-	loop_suffix='.bam'
-	loop_index_command="$samtools_path index /dev/stdin \$wd/\$rootname.bai"
-	loop_touch_command='touch $wd/$rootname.bai'
-	compression_level=9
-fi
-
 
 genome_tmp_dir=$(mktemp -d --suffix .align_smart-3seq.genome)
 cd $genome_tmp_dir
 echo -n 'loading genome into shared memory... ' >&2
 $star_path --genomeLoad LoadAndExit --genomeDir $genome_dir > /dev/null
+cd $wd
 echo 'done' >&2
 
 
-tmp_bam_files=()
 for fastq_file in "$@"
 do
-	cd $wd
 	rootname=$(basename $fastq_file .fastq.gz)
 	fastq=$(readlink -f $fastq_file)
-	outbam="$wd/$rootname$loop_suffix"
 	
 	echo -n "aligning $rootname... " >&2
 	
@@ -108,13 +86,12 @@ do
 	$unzip_path $fastq |
 		$umi_trim_path ${umi_trim_options[@]} $truncate_arg 2> $wd/$rootname.trim.log |
 		$star_path ${star_options[@]} --genomeDir $genome_dir |
-		tee $outbam |
-		eval $loop_index_command
-	eval $loop_touch_command
+		tee $wd/$rootname.bam |
+		$samtools_path index /dev/stdin $wd/$rootname.bai
+	touch $wd/$rootname.bai # ensure the index is younger than the BAM to avoid warnings
 	cp Log.final.out $wd/$rootname.align.log
 	cd $wd
 	rm -rf $tmp_dir
-	tmp_bam_files+=($outbam)
 	
 	echo 'done' >&2
 done
@@ -123,17 +100,4 @@ cd $genome_tmp_dir
 $star_path --genomeLoad Remove --genomeDir $genome_dir > /dev/null
 cd $wd
 rm -rf $genome_tmp_dir
-
-if $dedup
-then
-	echo 'deduplicating alignments... ' >&2
-	$parallel_path "
-		$dedup_command {} 2> {.}.dedup.log |
-			tee {.}.bam |
-			$samtools_path index /dev/stdin {.}.bai
-		rm {}
-		touch {.}.bai
-	" ::: ${tmp_bam_files[*]}
-	echo 'all done!' >&2
-fi
 
