e43a7bbcbed7955b27638b1024cfd40467e489c6
Joe Foley <jwfoley@stanford.edu>
2022-01-25 13:29:05 -0800

    replace umi_homopolymer.py with extract_umi.py

--- extract_umi.py
+++ extract_umi.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env pypy3
 
 from __future__ import print_function
 import argparse, sys
@@ -44,26 +44,21 @@ def writefq (name, seq, qualities):
 
 
 # parse arguments
-parser = argparse.ArgumentParser(description = 'for Smart-3SEQ reads in FASTQ format starting with a UMI and then a G-overhang (e.g. 5\'-NNNNNGGG-3\'), cut off both, adding the UMI to the read name and discard the G\'s; also trim 3\' homopolymers (e.g. polyA tails) and whatever follows them', formatter_class = argparse.ArgumentDefaultsHelpFormatter)
+parser = argparse.ArgumentParser(description = 'for Smart-3SEQ reads in FASTQ format starting with a UMI and then a G-overhang (e.g. 5\'-NNNNNGGG-3\'), cut off both, adding the UMI to the read name and discarding the G\'s', formatter_class = argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('-u', '--umi', action = 'store', type = int, default = 5, help = 'length of UMI')
 parser.add_argument('-g', '--g_overhang', action = 'store', type = int, default = 3, help = 'length of G-overhang')
-parser.add_argument('-b', '--homopolymer_base', action = 'store', type = str, default = 'A', help = 'base to look for in 3\' homopolymer')
-parser.add_argument('-p', '--homopolymer_length', action = 'store', type = int, default = 8, help = 'minimum homopolymer length for detecting a stretch to trim; set to 0 to disable homopolymer trimming')
-parser.add_argument('-m', '--mismatches', action = 'store', type = int, default = 1, help = 'number of allowed mismatches in stretch of A\'s')
-parser.add_argument('-n', '--no_trim', action = 'store_true', help = 'report trimmed lengths but leave homopolymer tails intact (better for aligners with soft-clipping)')
 parser.add_argument('-l', '--min_length', action = 'store', type = int, default = 1, help = 'minimum length of sequences to keep (after trimming)')
 parser.add_argument('-L', '--truncate_length', action = 'store', type = int, default = 0, help = 'length to truncate sequences (before trimming), 0 for no truncation')
 parser.add_argument('--bcl2fastq_trim_length', action = 'store', type = int, default = 35, help = 'length of the all-N reads generated when bcl2fastq trims below the threshold (--minimum-trimmed-read-length)')
 parser.add_argument('infile', action = 'store', nargs = '?', type = argparse.FileType('r'), default = sys.stdin)
 parser.add_argument('outfile', action = 'store', nargs = '?', type = argparse.FileType('w'), default = sys.stdout)
 args = parser.parse_args()
-assert(len(args.homopolymer_base) == 1)
 
 header_length = args.umi + args.g_overhang
 
 read_counter = 0
 overtrimmed_counter = 0
-barcode_counter = [Counter() for i in range(args.umi)]
+umi_counter = [Counter() for i in range(args.umi)]
 read_length_counter = Counter()
 for name, seq, qualities in readfq(args.infile):
 	read_counter += 1
@@ -77,44 +72,32 @@ for name, seq, qualities in readfq(args.infile):
 	if args.truncate_length != 0:
 		seq = seq[:args.truncate_length]
 		qualities = qualities[:args.truncate_length]
-
-	# get UMI
-	barcode = seq[0:args.umi]
-	for i in range(len(barcode)): barcode_counter[i][barcode[i]] += 1
 	
-	# find A stretch
-	polya_pos = len(seq)
-	if not args.homopolymer_length == 0:
-		for pos in range(header_length, len(seq) - header_length - args.homopolymer_length + 1):
-			if seq[pos] == args.homopolymer_base and seq[pos:(pos + args.homopolymer_length)].count(args.homopolymer_base) >= args.homopolymer_length - args.mismatches:
-				polya_pos = pos
-				break
-	read_length_counter[polya_pos - header_length] += 1
-	if args.no_trim: polya_pos = len(seq) # reset to full length now that it's been recorded
+	read_length_counter[len(seq)- header_length] += 1
 	
-	# write trimmed read
-	if polya_pos >= header_length + args.min_length: # don't write if too short
-		if args.umi > 0:
-			name += ':' + str(seq[0:args.umi])
-		seq = seq[header_length:polya_pos]
-		qualities = qualities[header_length:polya_pos]
-		args.outfile.write(writefq(name, seq, qualities))
+	if len(seq) - header_length >= args.min_length:
+		# get UMI
+		umi_seq = seq[0:args.umi]
+		for i in range(len(umi_seq)): umi_counter[i][umi_seq[i]] += 1
+		
+		# write trimmed read
+		seq = seq[header_length:]
+		name += ':' + umi_seq
+		args.outfile.write(writefq(name, seq, qualities[header_length:]))
 
 # print read count
 print('%i reads processed\n%i trimmed below threshold by bcl2fastq\n' % (read_counter, overtrimmed_counter), file = sys.stderr)
 
-# print barcode base frequency
-if args.umi > 0:
-	alphabet = sorted(set.union(*(set(i) for i in barcode_counter)))
-	print('UMI base frequency by position', file = sys.stderr)
-	print('\t'.join(alphabet), file = sys.stderr)
-	for pos in barcode_counter:
-		print('\t'.join(str(pos[base]) for base in alphabet), file = sys.stderr)
-	print('', file = sys.stderr)
+# print UMI base frequency
+alphabet = sorted(set.union(*(set(i) for i in umi_counter)))
+print('UMI base frequency by position', file = sys.stderr)
+print('\t'.join(alphabet), file = sys.stderr)
+for pos in umi_counter:
+	print('\t'.join(str(pos[base]) for base in alphabet), file = sys.stderr)
+print('', file = sys.stderr)
 
 # print insert length frequency
-if not args.homopolymer_length == 0:
-	print('trimmed read length counts', file = sys.stderr)
-	for (length, count) in sorted(read_length_counter.items()):
-		print('%i\t%i' % (length, count), file = sys.stderr)
+print('trimmed read length counts', file = sys.stderr)
+for (length, count) in sorted(read_length_counter.items()):
+	print('%i\t%i' % (length, count), file = sys.stderr)
 
