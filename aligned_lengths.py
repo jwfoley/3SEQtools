1c3dc4608672dadc3604456486e950ec3963023c
Joe Foley <jwfoley@stanford.edu>
2022-05-11 17:27:34 -0700

    check flags

--- aligned_lengths.py
+++ aligned_lengths.py
@@ -6,7 +6,13 @@ from collections import Counter
 length_counts = Counter()
 
 align_file = pysam.AlignmentFile('-')
-for alignment in align_file: length_counts[alignment.query_alignment_end] += 1
+for alignment in align_file: 
+	if not (
+		alignment.is_unmapped or
+		alignment.is_supplementary or
+		alignment.is_secondary or
+		alignment.is_qcfail
+	): length_counts[alignment.query_alignment_end] += 1
 
 print('length\treads')
 for (length, count) in sorted(length_counts.items()):
